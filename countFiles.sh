2802a6ddbfbf376244ab77870ff086a5923f41e7
Anweshan Bor <anweshanbor@gmail.com>
2018-12-25 11:00:57 +0530

    Script 1: major correction, changed PATH to PATHD

--- countFiles.sh
+++ countFiles.sh
@@ -1,24 +1,25 @@
-#!bin/bash
+#!/bin/sh
 
-PATH=
-EXT=
-
-if [ $# -eq $((0)) ]; then
+if [ "$#" -eq "0" ]; then
 	echo "Path of Directory not specified"
 	exit 1
-elif [ $# -eq $((1)) ]; then
-	PATH = $1
-	EXT = ".*"
-elif [ $# -eq $((2)) ]; then
-	PATH = $1
-	EXT = "$2"
-elif [ $# -gt $((2)) ]; then
+elif [ "$#" -eq "1" ]; then
+	PATHD=$1
+	EXT=".*"
+elif [ "$#" -eq "2" ]; then
+	PATHD=$1
+	EXT=$2
+elif [ "$#" -gt "2" ]; then
 	echo "More than expected arguments"
 	exit 2
 fi
 
-cd PATH
-ls -l * + EXT | wc -l
+path="$PATHD/*$EXT"
+ls $path | wc -l
+#echo `ls "$path" | wc -l`
+#count=$`ls $path | wc -l`
+#echo $count
+ 
 exit 0
 	
 
