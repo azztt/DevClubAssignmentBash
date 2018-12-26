f06596a6a9957233e5a885c095e851b4df4cc1bf
Anweshan Bor <anweshanbor@gmail.com>
2018-12-26 12:32:12 +0530

    Script 2: Finally added exit status and checked errors

--- uname.sh
+++ uname.sh
@@ -1,3 +1,42 @@
 #!/bin/sh
 
+file=$2
+uname=$1
+IFS=":"
 
+if [ $# -eq 0 ]; then
+	echo "Too few arguments! username, file missing"
+	echo "USE: ./uname.sh username file"
+	exit 1
+elif [ $# -eq 1 ]; then
+	echo "Too few arguments! file missing"
+	echo "USE: ./uname.sh username file"
+	exit 1
+fi
+
+if [ -f "$file" ]; then
+	while read line
+	do
+		index=0
+		for w in $line
+		do
+			if [ $((index % 7)) -eq 0 ]; then
+				index1=0
+				if [ $w = $uname ]; then
+					for w1 in $line
+					do
+						if [ $index1 -eq 4 ]; then
+							echo $w1
+						fi
+						index1=$((index1 + 1))
+					done
+				fi
+			fi
+			index=$((index + 1))
+		done
+	done < $file
+	exit 0
+else
+	echo "File not found!"
+	exit 1
+fi
